5bb619b270ce0a40f65d926cbb470912681a704e
irfanripat <irfanripat@gmail.com>
2021-03-21 00:09:20 +0800

    error handling in MainActivity.kt

--- app/src/main/java/com/irfan/githubuser/util/Constant.kt
+++ app/src/main/java/com/irfan/githubuser/util/Constant.kt
@@ -2,5 +2,5 @@ package com.irfan.githubuser.util
 
 object Constant {
     const val BASE_URL = "https://github.com/"
-    const val TOKEN = "ce9a45a44919b802fe67fc11c84cff78302267cc"
+    const val TOKEN = "YOUR PERSONAL ACCESS TOKEN"
 }
