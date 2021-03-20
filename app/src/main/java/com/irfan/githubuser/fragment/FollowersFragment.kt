c0263c6ff02939047bd709e11317d4e0c6d43aac
irfanripat <irfanripat@gmail.com>
2021-03-20 21:02:50 +0800

    fix scrolling behavior in view pager

--- app/src/main/java/com/irfan/githubuser/fragment/FollowersFragment.kt
+++ app/src/main/java/com/irfan/githubuser/fragment/FollowersFragment.kt
@@ -9,6 +9,7 @@ import com.irfan.githubuser.R
 
 class FollowersFragment : Fragment() {
 
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
